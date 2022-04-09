ceb66b0a0008a4176c78352cb90706e30715db4c
coolsneep <86134994+coolsneep@users.noreply.github.com>
2022-04-09 17:05:12 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function preload(){
 function setup(){
 
 database = firebase.database();
-createCanvas(windowWidth,windowHeight)
+createCanvas(2660,1360)
 frameRate(30)
 nameInput = createInput("");
 nameInput.input(load);
@@ -44,28 +44,28 @@ function draw(){
 
 if(gameState === 0){
 
-    nameInput.position(windowWidth/3,windowHeight/3)
-    playButton.position(windowWidth/3+200,windowHeight/3)
+    nameInput.position(2660/3,1360/3)
+    playButton.position(2660/3+200,1360/3)
     fill("white")
     background(map)
     textFont(fontBold,30);
 
     textAlign(CENTER);
     textSize(50)
-    text("rules:",windowWidth/8,windowHeight/2.3);
+    text("rules:",2660/8,1360/2.3);
     textFont(fontBold,23);
-    text("To start the game, enter your name an click on play",windowWidth/8,windowHeight/2.3+40);
-    text("You will be given a fact",windowWidth/8,windowHeight/2.3+80);
-    text("You must enter the country that it describes",windowWidth/8,windowHeight/2.3+120)
-    text("You have 3 tries to guess the correct country",windowWidth/8,windowHeight/2.3+160)
-    text("After each round, the location of the country will appear on a map",windowWidth/8,windowHeight/2.3+200)
-    text("You will do this for 10 different random countries",windowWidth/8,windowHeight/2.3+240)
-    text("You will receive a score based on your performance",windowWidth/8,windowHeight/2.3+280)
+    text("To start the game, enter your name an click on play",2660/8,1360/2.3+40);
+    text("You will be given a fact",2660/8,1360/2.3+80);
+    text("You must enter the country that it describes",2660/8,1360/2.3+120)
+    text("You have 3 tries to guess the correct country",2660/8,1360/2.3+160)
+    text("After each round, the location of the country will appear on a map",2660/8,1360/2.3+200)
+    text("You will do this for 10 different random countries",2660/8,1360/2.3+240)
+    text("You will receive a score based on your performance",2660/8,1360/2.3+280)
     textAlign(LEFT);
 
     textFont(fontBold,30);
     textSize(60)
-    text("Enter Your Name",windowWidth/3,windowHeight/4)
+    text("Enter Your Name",2660/3,1360/4)
 
 
 } 
@@ -86,8 +86,8 @@ if(gameState === 1){
     gameState = 1.5
     
     
-    temp.position(windowWidth/16,windowHeight/3)
-    temp2.position(windowWidth/16+150,windowHeight/3)
+    temp.position(2660/16,1360/3)
+    temp2.position(2660/16+150,1360/3)
    
     
     
@@ -109,12 +109,12 @@ if(gameState === 1.5){
 
     background(map)
     textFont(fontBold,40);
-    text(`Your Fact Is: ${value}`,windowWidth/15,windowHeight-100)
-    text(`Guesses made: ${guesses}`,windowWidth/1.5,850)
-    text(`Number of rounds played: ${roundsPlayed}`,windowWidth/1.5,800)
+    text(`Your Fact Is: ${value}`,2660/15,1360-100)
+    text(`Guesses made: ${guesses}`,2660/1.5,850)
+    text(`Number of rounds played: ${roundsPlayed}`,2660/1.5,800)
     textFont(fontBold,20);
-    text(`Wrong guesses ${wrong}`,windowWidth/15,windowHeight-400)
-    text(`Right guesses ${correct}`,windowWidth/15,windowHeight-350)
+    text(`Wrong guesses ${wrong}`,2660/15,1360-400)
+    text(`Right guesses ${correct}`,2660/15,1360-350)
     text(`score ${start -seconds-wrong*5+correct*5}`,100,200)
     // text(`${mouseX} ${mouseY}`,400,400)
     fill("red")
@@ -283,7 +283,7 @@ if(gameState === 3){
 textFont(fontBold,40);
 noStroke()
 fill("white")
-text(`Your name is: ${names}`,windowWidth-600,windowHeight/8)
+text(`Your name is: ${names}`,2660-600,1360/8)
 }
 
 
@@ -358,4 +358,4 @@ if(keyCode === 13)
     
 }
 
-}
+} 
